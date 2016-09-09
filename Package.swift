9f85a1d46aa4c9a79a845e95412ec0f0e3e18a3e
Igor Vojinovic <igor.vojinovic@appculture.com>
2016-09-09 14:48:22 +0200

    Updated package.swift for dependecies

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "Clock",
-    testDependencies: [
+    dependencies: [
     .Package(url: "https://github.com/neonichu/spectre-build.git", majorVersion: 0),
     ]
 )
